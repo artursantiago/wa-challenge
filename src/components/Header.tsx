f725e59611b1f28751671baad8b6eda64e416c05
Artur Santiago <artursantiago2001@hotmail.com>
2021-08-11 13:09:37 -0300

    feat: create Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,30 @@
+/**
+ * React & libs
+ */
+import React from 'react'
+import { AppBar, IconButton, Toolbar, Typography } from '@material-ui/core'
+import { NightsStay, WbSunny } from '@material-ui/icons'
+
+/**
+ * Config, core, components, utils, assets, styles
+ */
+import { useTheme } from 'core/hooks'
+
+export function Header(): JSX.Element {
+  const { theme, setTheme } = useTheme()
+
+  function handleSwitchTheme(): void {
+    setTheme(theme === 'dark' ? 'light' : 'dark')
+  }
+
+  return (
+    <AppBar>
+      <Toolbar>
+        <Typography variant="h4">Form</Typography>
+        <IconButton onClick={handleSwitchTheme} style={{ marginLeft: 'auto' }}>
+          {theme === 'dark' ? <WbSunny /> : <NightsStay />}
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  )
+}
--- src/core/contexts/ThemeContext.tsx
+++ src/core/contexts/ThemeContext.tsx
@@ -1,19 +1,26 @@
-import React, { createContext, useState } from 'react'
+/**
+ * React & libs
+ */
+import React, { createContext, useMemo, useState } from 'react'
+import { CssBaseline } from '@material-ui/core'
 import { ThemeProvider as ThemeProviderMUI } from '@material-ui/styles'
 import { createTheme } from '@material-ui/core/styles'
-// eslint-disable-next-line import/no-unresolved
-import 'fontsource-roboto'
+import '@fontsource/roboto'
 
 export const ThemeContext = createContext({} as ThemeContext.Data)
 
 export function ThemeProvider({ children }: ThemeContext.Props): JSX.Element {
   const [themeType, setThemeType] = useState<ThemeContext.Theme>('light')
 
-  const theme = createTheme({
-    palette: {
-      type: themeType
-    }
-  })
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          type: themeType
+        }
+      }),
+    [themeType]
+  )
 
   return (
     <ThemeContext.Provider
@@ -22,7 +29,10 @@ export function ThemeProvider({ children }: ThemeContext.Props): JSX.Element {
         setTheme: setThemeType
       }}
     >
-      <ThemeProviderMUI theme={theme}>{children}</ThemeProviderMUI>
+      <ThemeProviderMUI theme={theme}>
+        {children}
+        <CssBaseline />
+      </ThemeProviderMUI>
     </ThemeContext.Provider>
   )
 }
