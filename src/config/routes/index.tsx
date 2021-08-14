37d1f21c4a6ef81c61d001381927e22b51201b1c
Artur Santiago <artursantiago2001@hotmail.com>
2021-08-13 22:44:39 -0300

    refactor: use Router instead of BrowserRouter

--- src/config/routes/index.tsx
+++ src/config/routes/index.tsx
@@ -1,18 +1,20 @@
 import React, { Suspense } from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { Router, Route, Switch } from 'react-router-dom'
 import { CircularProgress } from '@material-ui/core'
 
 import { Home, Quiz } from 'pages'
 
+import history from './history'
+
 export const Routes = (): JSX.Element => {
   return (
-    <BrowserRouter>
+    <Router history={history}>
       <Suspense fallback={<CircularProgress />}>
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/quiz" component={Quiz} />
         </Switch>
       </Suspense>
-    </BrowserRouter>
+    </Router>
   )
 }
